f226a05e6a971b94ecb59c5c4e1238cea0446182
Sergey Shuktomov <s_shuktomov@mail.ru>
2021-02-21 17:44:28 +0300

    Initial Commit

--- somemart/settings.py
+++ somemart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from somemart import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ln%**8s&*g!gba@hoh&^bk6=aa1h9(&8$v_d2^c*h&nvo&pe&3'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
